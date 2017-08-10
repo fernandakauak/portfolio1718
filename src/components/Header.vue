8c1f5c465c2edd077e880c664c864ef13a95c364
Fernanda Kauak <fernanda.kauak@gmail.com>
2017-08-10 18:25:32 -0400

    Implementando rutas

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav id="sidebar-wrapper">
+        <ul class="sidebar-nav">
+            <a id="menu-close" href="#" class="btn btn-light btn-lg pull-right toggle"><i class="fa fa-times"></i></a>
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/about">Quien diseña</router-link></li>
+            <li><router-link>Servicios</router-link></li>
+            <li><router-link>Proyectos</router-link></li>
+            <li><router-link>Conversemos</router-link></li>
+        </ul>
+    </nav>
+
+</template>
--- src/components/top.vue
+++ src/components/top.vue
@@ -1,6 +1,19 @@
 <template>
     <header id="top" class="header">
         <div class="text-vertical-center">
+            <div style="height:80px; background-color:#CCC; clear:both">
+                <a id="menu-toggle" href="#" class="btn btn-dark btn-lg toggle"><i class="fa fa-bars"></i></a>
+                <nav id="sidebar-wrapper">
+                    <ul class="sidebar-nav">
+                        <a id="menu-close" href="#" class="btn btn-light btn-lg pull-right toggle"><i class="fa fa-times"></i></a>
+                        <li><router-link to="/">Home</router-link></li>
+                        <li><router-link to="/about">Quien diseña</router-link></li>
+                        <li><router-link to="/services">Servicios</router-link></li>
+                        <li><router-link to="/works">Proyectos</router-link></li>
+                        <li><router-link to="/contact">Conversemos</router-link></li>
+                    </ul>
+                </nav>
+            </div>
             <div class="col-md-offset-2 col-md-8 col-sm-12">
                 <img src="src/img/logo.png" />
             </div>
